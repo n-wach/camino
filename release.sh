2cb9972a1bf9b5eaef10222392c4aad928e40a05
n-wach <nmwachholz@gmail.com>
2023-01-10 08:56:00 -0800

    Include reminder of commands for tagging on release

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "Remember to tag and push the current commit!"
+# for instance:
+# git tag -a 1.0.4 -m "Release 1.0.4"
+# git push origin --tags
+
 python -m build
 read -p "Camino PyPI token: " token
 TWINE_USERNAME=__token__ TWINE_PASSWORD=$token twine upload dist/*
