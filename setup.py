db5518e9f8c7cf463203b1d552d09d6b2162d687
n-wach <nmwachholz@gmail.com>
2023-01-11 09:32:14 -0800

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="camino",
-    version="1.3.0",
+    version="1.3.1",
     author="Nathan Wachholz",
     author_email="camino@nathanwachholz.com",
     description="A library for controlling an Arduino from Python over Serial",
